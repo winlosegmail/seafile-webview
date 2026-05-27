c6f7d2807cc3d9a6b1ad5a970dd20b370f50f8fb
winlosegmail <joh.riot@gmail.com>
2026-05-27 20:16:27 -0300

    Create MainActivity.java

---
+++ app/src/main/java/com/seuapp/seafile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.seuapp.seafile;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView myWebView = new WebView(this);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("SEU_LINK_AQUI"); // <--- COLOQUE SEU IP/URL AQUI
+        setContentView(myWebView);
+    }
+}
